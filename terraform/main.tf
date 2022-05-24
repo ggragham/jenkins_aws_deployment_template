25c317743b5be17527014852d21157c0f9a4fa8b
Grell Gragham <vrt.epm@protonmail.com>
2022-05-24 23:19:39 +0300

    Fix: more restricted ssh acces

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "master" {
   ami                    = data.aws_ami.debian.id
   instance_type          = var.instance_type
   key_name               = var.master_key
-  vpc_security_group_ids = [aws_security_group.jenkins.id, aws_security_group.ssh.id]
+  vpc_security_group_ids = [aws_security_group.sg_master.id]
   tags                   = var.master_tags
 }
 
@@ -16,7 +16,7 @@ resource "aws_instance" "slave" {
   ami                    = data.aws_ami.debian.id
   instance_type          = var.instance_type
   key_name               = var.slave_key
-  vpc_security_group_ids = [aws_security_group.ssh.id]
+  vpc_security_group_ids = [aws_security_group.sg_slave.id]
   tags                   = var.slave_tags
   count                  = var.slave_count
 }
@@ -40,13 +40,20 @@ data "aws_vpc" "default" {
 
 # Security Group
 
-resource "aws_security_group" "ssh" {
-  name        = "ssh"
-  description = "Allow 20 port for ssh"
+resource "aws_security_group" "sg_master" {
+  name        = "sg_master"
+  description = "Security Group for Master node"
   vpc_id      = data.aws_vpc.default.id
 
   ingress {
-    description = "tcp 22"
+    description = "Jenkins WEB UI"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["${var.default_cidr_block}/32"]
+  }
+  ingress {
+    description = "SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -61,16 +68,24 @@ resource "aws_security_group" "ssh" {
   }
 }
 
-resource "aws_security_group" "jenkins" {
-  name        = "jenkins"
-  description = "Allow 8080 port for Jenkins"
+resource "aws_security_group" "sg_slave" {
+  name        = "sg_slave"
+  description = "Security Group for Slave nodes"
   vpc_id      = data.aws_vpc.default.id
 
   ingress {
-    description = "tcp 8080"
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["${var.default_cidr_block}/32"]
+    description     = "SSH"
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.sg_master.id]
+    cidr_blocks     = ["${var.default_cidr_block}/32"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 }
