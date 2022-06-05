ed9644dc7e56c19389bf5725c6ff8ecbf439d075
Grell Gragham <vrt.epm@protonmail.com>
2022-06-06 02:25:24 +0300

    Add: dockerize jenkins-cli

---
+++ jenkins_cli/get_jenkins_cli.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+JENKINS_CLI_URL=""
+
+if [[ -z $JENKINS_URL ]]; then
+    echo "JENKINS_URL env variable is not set"
+    exit 1
+else
+    JENKINS_CLI_URL="$JENKINS_URL/jnlpJars/jenkins-cli.jar"
+    echo "$JENKINS_CLI_URL"
+    curl "$JENKINS_CLI_URL" -o jenkins_cli.jar
+fi
+
+exec /bin/bash
